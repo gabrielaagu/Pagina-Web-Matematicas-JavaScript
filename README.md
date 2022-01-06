cbaee59ed0339060e1b87f3ea3341a52bda5d964
gabrielaauzcategui@gmail.com <gabrielaauzcategui@gmail.com>
2022-01-06 16:34:18 +0100

    segundo taller

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Pagina-Web-Matematicas-JavaScript
 Curso práctico de JS con Platzi:
 
-## Taller #1: Figuras geométricas
+## Taller #1: Figuras geométricas:
+
+- Primer paso: Definir las fórmulas.
+- Segundo paso: Implementar las fórmulas en JavaScript.
+- Tercer paso: Crear funciones.
+- Cuarto paso: Integrar JS con HTML.
+
+
+## Taller #2: Porcentajes y descuentos:
+
+- Primer paso: Definir las fórmulas.
+- Segundo paso: Implementar las fórmulas en JavaScript.
+- Tercer paso: Crear funciones.
+- Cuarto paso: Integrar JS con HTML.
+
+## Taller #2: Promedio, mediana y moda:
 
 - Primer paso: Definir las fórmulas.
 - Segundo paso: Implementar las fórmulas en JavaScript.
---
+++ descuentos.js
@@ -0,0 +1,23 @@
+// const precioOriginal = 100;
+// const descuento = 15;
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const result = document.getElementById("Result");
+    result.innerText = "El precio con descuento son: " + precioConDescuento + "€";
+}
